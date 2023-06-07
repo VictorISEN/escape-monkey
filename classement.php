5837b513d28c8036f783b51002771d81d5d806d4
Théo Meilliez <112801606+Theo-meil@users.noreply.github.com>
2023-06-08 00:05:34 +0200

    Classement terminé

--- classement.php
+++ classement.php
@@ -39,11 +39,19 @@
             </ul>
             <ul class="navbar-nav">
             <?php
-            if(isset($_COOKIE['email'])){
-                echo $_COOKIE['nom']; 
-                echo '<a class="nav-link" href="deconnexion.php">';
-                echo $_COOKIE['nom'];
+            if(isset($_COOKIE['email'])){ 
+                
+                echo '<li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#decoModal">';
+                echo '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-openvpn" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M15.618 20.243l-2.193 -5.602a3 3 0 1 0 -2.849 0l-2.193 5.603"></path>
+                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                </svg>  ';
+                echo $_COOKIE['pseudo'];
                 echo '</a>';
+                echo '</li>';
+                
             } else {
                 echo '<li class="nav-item">
                 <a class="nav-link" href="#" data-toggle="modal" data-target="#signupModal">Inscription</a>
@@ -68,6 +76,50 @@
 
     require 'connexion.php';
     ?>
+     <div class="modal fade" id="decoModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="loginModalLabel">Account</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fermer">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <form class="form" method="POST" action="deconnexion.php">
+                        <div class="form-group">
+                            <label for="loginEmail">Name: <?php echo $_COOKIE['nom']; ?></label>
+                        </div>
+                        <div class="form-group">
+                            <label for="loginEmail">Surname : <?php echo $_COOKIE['prenom']; ?></label>
+                        </div>
+                        <div class="form-group">
+                            <label for="loginPassword">Email : <?php echo $_COOKIE['email']; ?></label>
+                        </div>
+                        <div class="form-group">
+                            <label for="loginPassword">Highscore : <?php echo $_COOKIE['High_Score'];?></label>
+                        </div>
+                        <button type="submit" value="submit" name="submit" class="btn btn-primary">Deconnexion</button>
+                        <span class="error">* <?php echo $passwordErr;?></span><br>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "escape_monkey";
+
+    // Créer une connexion
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    ?>
         
         <!-- Identique à la barre de navigation de la page principale -->
     <section class="container my-5">
@@ -87,16 +139,25 @@
                     <tbody>
                         <!-- Insérez ici les rangs des joueurs sous la forme d'éléments de liste -->
                         <!-- Exemple: -->
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Player 1</td>
-                            <td>1234</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Player 2</td>
-                            <td>1176</td>
-                        </tr>
+                        <?php 
+                        $sql = "SELECT pseudo, High_Score FROM utilisateurs ORDER BY High_Score DESC";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            $rank = 1;
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                echo "<th scope='row'>" . $rank++ . "</th>";
+                                echo "<td>" . $row["pseudo"]. "</td>";
+                                echo "<td>" . $row["High_Score"]. "</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        $conn->close();
+                        ?>
                         <!-- Continuez à ajouter des rangs de joueurs ici -->
                     </tbody>
                 </table>
